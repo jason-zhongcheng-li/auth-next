c833719371b1e76779b10bc5d4af0e21dfe7237d
ZHONGCHENG LI <zhongcheng@192-168-1-102.tpgi.com.au>
2024-01-12 11:18:09 +1100

    feat: added login form, card-wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,36 @@
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { LoginButton } from "@/components/auth/login-button";
 
-export default function Home() {
+const font = Poppins({ subsets: ["latin"], weight: ["600"] });
+
+export default async function Home() {
   return (
-    <Button size="lg" variant="link">
-      Test
-    </Button>
+    <main
+      className="flex h-full flex-col items-center justify-center 
+    bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800"
+    >
+      <div className="space-y-6">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-center text-lg">
+          A simple authentication service
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
