a9dae3711e7f503cd29e679151205d378a12a0ec
ZHONGCHENG LI <zhongcheng@192-168-1-100.tpgi.com.au>
2024-02-03 01:17:45 +1100

    feat: implemented signIn in login action

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,30 @@
-import GitHub from "next-auth/providers/github";
-
+import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
+// use LoginSchema check in the providers below to avoid bypass API call for login
+import { LoginSchema } from "./schemas";
+import { getUserByEmail } from "./data/user";
+import bcrypt from "bcryptjs";
 
 export default {
-  providers: [GitHub],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validateFields = LoginSchema.safeParse(credentials);
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) {
+            return null;
+          }
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (passwordMatch) {
+            return user;
+          }
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
--- auth.ts
+++ auth.ts
@@ -4,9 +4,12 @@ import authConfig from "./auth.config";
 
 import { db } from "./lib/db";
 
+// these exported functions can be used in any component or server action
 export const {
   handlers: { GET, POST },
   auth,
+  signIn,
+  signOut,
 } = NextAuth({
   ...authConfig,
   adapter: PrismaAdapter(db),
