2ae084c0428a14b6783d0a83f2eec50263a451e0
ZHONGCHENG LI <zhongcheng@192-168-1-100.tpgi.com.au>
2024-02-04 12:14:30 +1100

    feat: added typescript support for user role in session

--- auth.ts
+++ auth.ts
@@ -1,8 +1,17 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "./auth.config";
 import { db } from "./lib/db";
 import { getUserById } from "./data/user";
+import { UserRole } from "@prisma/client";
+
+type ExtendedUser = DefaultSession["user"] & { role: UserRole };
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
 
 // these exported functions can be used in any component or server action
 export const {
@@ -25,11 +34,27 @@ export const {
     },
     // @ts-ignore
     async session({ session, token }) {
-      console.log("session token =", token);
+      if (session.user) {
+        if (token.sub) {
+          session.user.id = token.sub;
+        }
+        if (token.role) {
+          session.user.role = token.role as UserRole;
+        }
+      }
+      console.log({ sessionToken: token, session });
       return session;
     },
     async jwt({ token, account, profile }) {
-      // const user = await getUserById(token.sub);
+      /**
+       * All customized properties in this token can be used in middleware auth function
+       * where we can verify user role against different routes. It's role based action control in middleware
+       */
+      if (token.sub) {
+        const user = await getUserById(token.sub);
+        token.role = user?.role;
+      }
+
       return token;
     },
   },
